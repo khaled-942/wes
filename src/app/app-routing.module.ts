15692b474341fa7d2b20e57b4c3f391ed050caad
khaled <76748059+khaled-942@users.noreply.github.com>
2023-11-01 17:07:59 +0200

    routing & seprate components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ProgramComponent } from './program/program.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'speakers', loadChildren: () => import('./speakers/speakers.module').then((m) => m.SpeakersModule)},
+  { path: 'about', component: AboutComponent },
+  { path: 'programme', component: ProgramComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' },
--- src/app/speakers/speakers-routing.module.ts
+++ src/app/speakers/speakers-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SpeakersComponent } from './speakers/speakers.component';
+import { SpeakerDetailComponent } from './speaker-detail/speaker-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:SpeakersComponent},
+  {path:'speaker/:id',component:SpeakerDetailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
