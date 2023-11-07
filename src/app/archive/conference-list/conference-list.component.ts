3fe4b047d9ebdfbf8fd16f6a0791cf8a0d0dde7f
khaled <76748059+khaled-942@users.noreply.github.com>
2023-11-07 17:06:39 +0200

    archive

--- src/app/archive/conference-list/conference-list.component.ts
+++ src/app/archive/conference-list/conference-list.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Conference } from 'src/app/model/Conference';
+import { ApiServiceService } from 'src/app/services/api-service.service';
 
 @Component({
   selector: 'app-conference-list',
   templateUrl: './conference-list.component.html',
-  styleUrls: ['./conference-list.component.scss']
+  styleUrls: ['./conference-list.component.scss'],
 })
 export class ConferenceListComponent {
-  conferences = [1, 1, 1, 1, 1];
+  conferences!: Conference[];
+  constructor(private api: ApiServiceService) {}
+  ngOnInit(): void {
+    this.api.getconference().subscribe((data: any) => {
+      console.log(data.conferences)
+      this.conferences = data.conferences;
+    });
+  }
 }
--- src/app/archive/conference/conference.component.ts
+++ src/app/archive/conference/conference.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Conference } from 'src/app/model/Conference';
 
 @Component({
   selector: 'app-conference',
   templateUrl: './conference.component.html',
-  styleUrls: ['./conference.component.scss']
+  styleUrls: ['./conference.component.scss'],
 })
 export class ConferenceComponent {
-
+  @Input() eachConference!: Conference;
+  constructor(private route:Router){}
+  navigateTo(destination:string){
+    this.route.navigate([`/${destination}`])
+  }
 }
