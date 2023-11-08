f6934156984cf9147b1b83e87a4048fec9b2a9f6
khaled <76748059+khaled-942@users.noreply.github.com>
2023-11-08 10:57:11 +0200

    dynamic archive data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from '../services/api-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  confDareArr: Array<any> = [];
+  constructor(private api: ApiServiceService) {}
+  ngOnInit(): void {
+      this.api.getconference().subscribe((conferences: any) => {
+        conferences.conferences.map((conference: any) => {
+          this.confDareArr.push(new Date(conference.day_one_date).getTime());
+        });
+        const currentConferenceDate = conferences.conferences.filter((getLastConf:any)=> new Date(getLastConf.day_one_date).getTime() === Math.max(...this.confDareArr))[0]._id;
+        this.api.confId.next(currentConferenceDate);
+      })
+  }
 }
